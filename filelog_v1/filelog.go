3216953eb4c0218c6513cc20e87a26a8a00e6ebb
DESKTOP-A6285B0\zhoujian <24086215@qq.com>
2021-12-29 16:51:17 +0800

    CFileLog结构暴露出去

--- filelog_v1/filelog.go
+++ filelog_v1/filelog.go
@@ -17,7 +17,7 @@ type UData struct {
 }
 
 // 结构体2： 日志操作结构体
-type uFileLog struct {
+type CFileLog struct {
 	AutoId  int64            // 流水id，从0开始
 	indexFp *os.File         // 索引文件句柄
 	dataFps map[int]*os.File // 日志数据文件句柄数组，键值为数据文件编号
@@ -29,8 +29,8 @@ type uFileLog struct {
 }
 
 // 函数1：获取日志操作实例
-func New(Folder string, Date string) *uFileLog {
-	return &uFileLog{
+func New(Folder string, Date string) *CFileLog {
+	return &CFileLog{
 		folderLog:   Folder,
 		date:        Date,
 		indexFp:     nil,
@@ -42,7 +42,7 @@ func New(Folder string, Date string) *uFileLog {
 }
 
 // 函数2：关闭释放
-func (Me *uFileLog) Close() {
+func (Me *CFileLog) Close() {
 	if Me.indexFp != nil {
 		_ = Me.indexFp.Close()
 	}
@@ -55,7 +55,7 @@ func (Me *uFileLog) Close() {
 }
 
 // 函数3：读取位置
-func (Me *uFileLog) GetAutoId() (int64, error) {
+func (Me *CFileLog) GetAutoId() (int64, error) {
 	if Me.AutoId == -2 {
 		return -1, errors.New("实例已关闭")
 	}
@@ -69,7 +69,7 @@ func (Me *uFileLog) GetAutoId() (int64, error) {
 }
 
 // 函数4：新增一条日志
-func (Me *uFileLog) Add(DataType int16, Data []byte) (int64, error) {
+func (Me *CFileLog) Add(DataType int16, Data []byte) (int64, error) {
 	if Me.AutoId == -2 {
 		return -1, errors.New("实例已关闭")
 	}
@@ -131,7 +131,7 @@ func (Me *uFileLog) Add(DataType int16, Data []byte) (int64, error) {
 }
 
 // 函数5：读取一条数据
-func (Me *uFileLog) GetOne(Id int64) (*UData, error) {
+func (Me *CFileLog) GetOne(Id int64) (*UData, error) {
 	if Me.AutoId == -2 {
 		return nil, errors.New("实例已关闭")
 	}
@@ -190,7 +190,7 @@ func (Me *uFileLog) GetOne(Id int64) (*UData, error) {
 }
 
 // 辅助函数1：初始化内容文件句柄
-func (Me *uFileLog) initFpData(index int) error {
+func (Me *CFileLog) initFpData(index int) error {
 	// 判断日期目录是否存在,没有将会创建
 	folderDate, err := Me.getLogDateFolder()
 	if err != nil {
@@ -212,7 +212,7 @@ func (Me *uFileLog) initFpData(index int) error {
 }
 
 // 辅助函数2：初始化自增id
-func (Me *uFileLog) initAutoId() error {
+func (Me *CFileLog) initAutoId() error {
 	if err := Me.initFpIndex(); err != nil {
 		return err
 	}
@@ -223,7 +223,7 @@ func (Me *uFileLog) initAutoId() error {
 }
 
 // 辅助函数3：初始化索引文件句柄
-func (Me *uFileLog) initFpIndex() error {
+func (Me *CFileLog) initFpIndex() error {
 	if Me.indexFp == nil {
 		// 判断日期目录是否存在
 		folderDate, err := Me.getLogDateFolder()
@@ -243,7 +243,7 @@ func (Me *uFileLog) initFpIndex() error {
 }
 
 // 辅助函数4：获取日志文件夹
-func (Me *uFileLog) getLogDateFolder() (string, error) {
+func (Me *CFileLog) getLogDateFolder() (string, error) {
 	folderDate := strings.TrimRight(Me.folderLog, "/") + "/" + strings.ReplaceAll(Me.date, "-", "")
 	if _, err := os.Stat(folderDate); os.IsNotExist(err) {
 		// 创建目录
@@ -256,7 +256,7 @@ func (Me *uFileLog) getLogDateFolder() (string, error) {
 }
 
 // 辅助函数5：读取指定长度数据
-func (Me *uFileLog) fileReadLength(fp *os.File, Length int) ([]byte, error) {
+func (Me *CFileLog) fileReadLength(fp *os.File, Length int) ([]byte, error) {
 	// 读取一批
 	var retBuff = make([]byte, 0)
 	for {
@@ -278,7 +278,7 @@ func (Me *uFileLog) fileReadLength(fp *os.File, Length int) ([]byte, error) {
 }
 
 // 数字类型转换1： int转[]byte
-func (Me *uFileLog) int2Bytes(n int) []byte {
+func (Me *CFileLog) int2Bytes(n int) []byte {
 	x := int32(n)
 
 	bytesBuffer := bytes.NewBuffer([]byte{})
@@ -287,28 +287,28 @@ func (Me *uFileLog) int2Bytes(n int) []byte {
 }
 
 // 数字类型转换2： int16转[]byte
-func (Me *uFileLog) int16ToBytes(i int16) []byte {
+func (Me *CFileLog) int16ToBytes(i int16) []byte {
 	var buf = make([]byte, 2)
 	binary.BigEndian.PutUint16(buf, uint16(i))
 	return buf
 }
 
 // 数字类型转换3： int32转[]byte
-func (Me *uFileLog) int32ToBytes(i int32) []byte {
+func (Me *CFileLog) int32ToBytes(i int32) []byte {
 	var buf = make([]byte, 4)
 	binary.BigEndian.PutUint32(buf, uint32(i))
 	return buf
 }
 
 // 数字类型转换4： int64转[]byte
-func (Me *uFileLog) int64ToBytes(i int64) []byte {
+func (Me *CFileLog) int64ToBytes(i int64) []byte {
 	var buf = make([]byte, 8)
 	binary.BigEndian.PutUint64(buf, uint64(i))
 	return buf
 }
 
 // 数字类型转换5： []byte转int
-func (Me *uFileLog) bytes2Int(b []byte) int {
+func (Me *CFileLog) bytes2Int(b []byte) int {
 	bytesBuffer := bytes.NewBuffer(b)
 
 	var x int32
@@ -319,16 +319,16 @@ func (Me *uFileLog) bytes2Int(b []byte) int {
 }
 
 // 数字类型转换6： []byte转int16
-func (Me *uFileLog) bytes2Int16(buf []byte) int16 {
+func (Me *CFileLog) bytes2Int16(buf []byte) int16 {
 	return int16(binary.BigEndian.Uint16(buf))
 }
 
 // 数字类型转换7： []byte转int32
-func (Me *uFileLog) bytes2Int32(buf []byte) int32 {
+func (Me *CFileLog) bytes2Int32(buf []byte) int32 {
 	return int32(binary.BigEndian.Uint32(buf))
 }
 
 // 数字类型转换8： []byte转int64
-func (Me *uFileLog) bytes2Int64(buf []byte) int64 {
+func (Me *CFileLog) bytes2Int64(buf []byte) int64 {
 	return int64(binary.BigEndian.Uint64(buf))
 }
